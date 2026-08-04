6224361be5bc420b13abf9bd3adac10dbd1d41cd
kiddtobal <cristobaliribarrenrojas@gmail.com>
2026-08-04 14:26:52 -0400

    Publicar

--- VidrieriaPresupuestos.Web/Program.cs
+++ VidrieriaPresupuestos.Web/Program.cs
@@ -14,6 +14,8 @@ CultureInfo.DefaultThreadCurrentUICulture = culturaApp;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Host.UseWindowsService();
+
 // Add services to the container.
 builder.Services.AddDbContext<VidrieriaContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("VidrieriaDatabase")));
@@ -29,11 +31,8 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
 }
 app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
-app.UseHttpsRedirection();
 
 app.UseAntiforgery();
 
